0d2905cda0b5047aa80262bbe060550e3f32d697
Muthukumar <muthukumarperiyasamy.mk@gmail.com>
2021-08-22 23:02:00 +0530

    Feat: create a function to get blog post data and blog post list

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,22 +1,31 @@
 import {LoaderFunction} from 'remix'
 import {useLoaderData} from '@remix-run/react'
+import {Link} from 'react-router-dom'
 
-import {getContent} from '../utils/mdx.server'
-import {useMdxComponent} from '../utils/hooks'
+import {BlogPostListType, getBlogPostsList} from '../utils/mdx.server'
+// import {useMdxComponent} from '../utils/hooks'
 
 export const loader: LoaderFunction = async () => {
-  const {code} = await getContent({slug: 'avoid-magic-numbers.mdx'})
-  return code
+  // const {code} = await getContent({slug: 'avoid-magic-numbers.mdx'})
+  // console.log(getBlogPostsList())
+  return getBlogPostsList()
 }
 
 export default function Index() {
-  const data = useLoaderData()
+  const blogPostList = useLoaderData<Array<BlogPostListType>>()
 
-  const Component = useMdxComponent(data)
+  // const Component = useMdxComponent(data)
+  console.log(blogPostList)
 
   return (
     <div className="text-primary">
-      <Component />
+      {blogPostList.map((blog) => {
+        return (
+          <div key={blog.slug}>
+            <Link to={`/blog/${blog.slug}`}>{blog.title}</Link>
+          </div>
+        )
+      })}
     </div>
   )
 }
--- app/utils/mdx.server.ts
+++ app/utils/mdx.server.ts
@@ -1,19 +1,77 @@
 import fs from 'fs'
 import path from 'path'
 import {bundleMDX} from 'mdx-bundler'
+import GrayMatter from 'gray-matter'
+
+export interface BlogPostType {
+  title: string
+  date: string
+  category: Array<string>
+  updatedAt: string
+  draft: boolean
+  description: string
+}
+
+export interface BlogPostListType extends BlogPostType {
+  slug: string
+}
+
+const BLOG_FOLDER_PATH = path.join(process.cwd(), 'content', 'blog')
 
 const getContent = async ({slug}: {slug: string}) => {
   const contentPath = path.join(process.cwd(), 'content', 'blog', slug)
-  const indexFilePath = path.join(process.cwd(), 'content', 'blog', 'index.tsx')
+  // const indexFilePath = path.join(process.cwd(), 'content', 'blog', 'index.tsx')
 
   const mdxSource = String(fs.readFileSync(contentPath))
-  const indexFileData = String(fs.readFileSync(indexFilePath))
+  // const indexFileData = String(fs.readFileSync(indexFilePath))
 
   return await bundleMDX(mdxSource, {
-    files: {
-      'index.tsx': indexFileData,
-    },
+    // files: {
+    //   'index.tsx': indexFileData,
+    // },
   })
 }
 
-export {getContent}
+function getBlogData(blogPath: string): BlogPostType {
+  if (typeof blogPath !== 'string') {
+    throw new Error('Blog path should be a string.')
+  }
+
+  const blogData = fs.readFileSync(blogPath)
+
+  const {
+    title = '',
+    date = '',
+    category = [],
+    updatedAt = '',
+    draft = true,
+    description = '',
+  } = GrayMatter(blogData).data
+
+  return {title, date, category, updatedAt, draft, description}
+}
+
+function getBlogPostsList(): Array<BlogPostListType> {
+  const blogFolderStat = fs.lstatSync(BLOG_FOLDER_PATH)
+
+  if (!blogFolderStat.isDirectory()) {
+    throw new Error('Passed blog folder path is not a directory.')
+  }
+
+  const blogPosts = fs.readdirSync(BLOG_FOLDER_PATH)
+
+  return blogPosts.map((blogPostName) => {
+    const blogPath = path.join(BLOG_FOLDER_PATH, blogPostName)
+
+    const blogStat = fs.lstatSync(blogPath)
+
+    if (blogStat.isDirectory()) {
+      const nestedBlogPath = path.join(blogPath, 'index.mdx')
+      return {...getBlogData(nestedBlogPath), slug: blogPostName}
+    }
+
+    return {...getBlogData(blogPath), slug: blogPostName.replace(/.mdx/g, '')}
+  })
+}
+
+export {getContent, getBlogPostsList}
