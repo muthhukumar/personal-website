086f4ee3d8cfa63118ed0100582936ec1c347a4c
Muthukumar <muthukumarperiyasamy.mk@gmail.com>
2021-08-25 22:22:24 +0530

    feat: add node env stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  mode: 'jit',
+  mode: process.env.NODE_ENV ? 'jit' : undefined,
   purge: ['./app/**/*', './app/routes/**/*', './app/components/**/*'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     fontFamily: {
-      'sans': ['Roboto', ...defaultTheme.fontFamily.sans],
       ...defaultTheme.fontFamily,
+      'sans': ['Roboto', ...defaultTheme.fontFamily.sans],
     },
   },
   variants: {
