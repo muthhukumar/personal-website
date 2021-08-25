b7214a1b0c4c5ed865e9c418f37bef42c44055b9
Muthukumar <muthukumarperiyasamy.mk@gmail.com>
2021-08-25 22:22:55 +0530

    refactor: cleanups

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,14 +1,6 @@
-import {LoaderFunction} from 'remix'
-
-import {getBlogPostListFromDisk} from '../utils/mdx.server'
-
-export const loader: LoaderFunction = async () => {
-  return getBlogPostListFromDisk()
-}
-
 export default function Index() {
   return (
-    <div className="w-full p-16 pt-0 mt-32">
+    <div className="p-16 pt-0 mt-32">
       <div className="container max-w-5xl mx-auto mt-12 text-primary">
         <div className="text-xl font-normal">Hi, I&apos;m </div>
         <h1 className="font-bold text-8xl">Muthukumar</h1>
