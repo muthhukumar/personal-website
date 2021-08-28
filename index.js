f721514b78138f2e5ace6e65cd5d2bc65395440a
Muthukumar <muthukumarperiyasamy.mk@gmail.com>
2021-08-28 22:12:21 +0530

    fix: increase cache time for fonts and images

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.use(compression())
 // You may want to be more aggressive with this caching
 app.use(express.static('public', {maxAge: '1h'}))
 
+app.use(express.static('public/fonts', {maxAge: '1y'}))
+
+app.use(express.static('public/images', {maxAge: '1y'}))
+
 // Remix fingerprints its assets so we can cache forever
 app.use(express.static('public/build', {immutable: true, maxAge: '1y'}))
 
