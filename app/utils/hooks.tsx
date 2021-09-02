a30dd58d9e69fe16d427ee81c1eea3f22d278cbd
Muthukumar <muthukumarperiyasamy.mk@gmail.com>
2021-09-02 21:48:39 +0530

    feat: initial prism react renderer setup

--- app/utils/hooks.tsx
+++ app/utils/hooks.tsx
@@ -9,9 +9,9 @@ function Highlight(props) {
       {({className, style, tokens, getLineProps, getTokenProps}) => (
         <pre className={className} style={style}>
           {tokens.map((line, i) => (
-            <div {...getLineProps({line, key: i})}>
+            <div {...getLineProps({line})} key={i}>
               {line.map((token, key) => (
-                <span {...getTokenProps({token, key})} />
+                <span {...getTokenProps({token})} key={key} />
               ))}
             </div>
           ))}
