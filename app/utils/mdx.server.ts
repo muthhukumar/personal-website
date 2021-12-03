638b21aa4c6ecc782535ea38e29475cffea511b0
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2021-12-03 13:13:05 +0530

    fix: folder structure

--- app/utils/mdx.server.ts
+++ app/utils/mdx.server.ts
@@ -4,7 +4,7 @@ import matter from 'gray-matter'
 // import remark from 'remark'
 // import html from 'remark-html'
 
-const postsDirectory = join(process.cwd(), 'content')
+const postsDirectory = join(process.cwd(), '_content')
 
 export function getPostSlugs() {
   return fs.readdirSync(postsDirectory)
