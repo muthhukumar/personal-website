b58e558c92f1583d55c27db080d40a71f8caac8a
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2021-12-03 13:25:49 +0530

    fix: update path

--- app/utils/mdx.server.ts
+++ app/utils/mdx.server.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 import { join } from 'path'
 import matter from 'gray-matter'
 
-const postsDirectory = join(process.cwd(), 'app', 'content')
+const postsDirectory = join(__dirname, '..', '..', 'content')
 
 export function getPostSlugs() {
   return fs.readdirSync(postsDirectory)
