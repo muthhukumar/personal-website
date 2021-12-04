ededa4bb994e108a25f3b376ae49a15aca86812d
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2021-12-04 10:57:46 +0530

    feat: udpate content path

--- app/utils/mdx.server.ts
+++ app/utils/mdx.server.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 import { join } from 'path'
 import matter from 'gray-matter'
 
-const postsDirectory = join(process.cwd(), 'app', 'routes', 'content')
+const postsDirectory = join(process.cwd(), 'content')
 
 export function getPostSlugs() {
   return fs.readdirSync(postsDirectory)
