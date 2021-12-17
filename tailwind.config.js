10951d10cd67added7eb0e9e6e768f587eccf242
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2021-12-17 20:28:08 +0530

    updated tailwindcss stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   mode: 'jit',
-  purge: ['./app/**/*.{ts,tsx}'],
+  content: ['./app/**/*.{ts,tsx}'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {
