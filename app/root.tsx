ef564bf72b611433d26d5b83be4ae40b1c0c1322
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2021-12-21 06:57:26 +0530

    feat(seo0: remove duplicate viewport meta tags

--- app/root.tsx
+++ app/root.tsx
@@ -158,7 +158,6 @@ function Document({ children, title }: { children: React.ReactNode; title?: stri
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <meta name="color-scheme" content="dark light" />
         {title ? <title>{title}</title> : null}
         <Meta />
