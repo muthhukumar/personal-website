d0ac0c0c2f6d89b9a2182f98937c0b9c25cbf472
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2021-12-22 18:33:18 +0530

    udpate some stuff

--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -1,5 +1,5 @@
 import { LoaderFunction, redirect } from 'remix'
 
 export const loader: LoaderFunction = () => {
-  throw redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+  throw redirect('/maygodhelpyou')
 }
