2e751417de440d930fb45e94e3080d7a6b09614f
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2022-01-29 13:25:25 +0530

    feat: fix themeng stuff

--- app/components/continue-reading.tsx
+++ app/components/continue-reading.tsx
@@ -2,10 +2,10 @@ export default function ContinueReading() {
   return (
     <div className="relative mt-10 mb-20 sm:mt-20">
       <div className="absolute inset-0 flex items-center" aria-hidden="true">
-        <div className="w-full border-t border-gray-300"></div>
+        <div className="w-full border-t border-color"></div>
       </div>
       <div className="relative flex justify-center">
-        <span className="px-2 text-sm text-gray-500 bg-white">Continue Reading</span>
+        <span className="px-2 text-sm text-gray-500 bg-color">Continue Reading</span>
       </div>
     </div>
   )
